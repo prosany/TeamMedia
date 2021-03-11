dbcbde99941c6ef15447504439844ebc759eeaf6
Mahabub Sunny <office.prosany@gmail.com>
2021-03-11 23:58:49 +0600

    Complete

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,9 +3,13 @@ import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            404 Error
-        </div>
+        <>
+            <div className="error">
+                <h1>Oops!</h1>
+                <h2>404 Page Not Found</h2>
+                <a href="/">Home</a>
+            </div>
+        </>
     );
 };
 
